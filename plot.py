79942c572f31507da1f8bfb37e77ce55241891ff
Fabien Roger <efab.roger@gmail.com>
2024-01-11 08:56:14 -0800

    Run pro vs anti AI

--- plot.py
+++ plot.py
@@ -10,24 +10,24 @@ methods = ["direct", "sentiment"]
 templates = {"fabien": "Long template", "minimal": "Minimal template"}
 
 # print male-female bias in a errorbar horizontal
-fig, axs = plt.subplots(1, 2, dpi=300, figsize=(6,2.5), sharey=True)
+fig, axs = plt.subplots(1, 2, dpi=300, figsize=(6, 2.5), sharey=True)
 
 for j, ax, method in zip(range(len(axs)), axs, methods):
     ax.set_title(f"Method: {method}")
     ax.xaxis.set_major_locator(MaxNLocator(nbins=2))
-    
+
     y_labels = []
     for i, model in enumerate(models):
         this_data = data[f"mf-{model}-{method}"]
         x = [d["mean"] for d in this_data.values()]
         x_err = [d["2-sigma-u"] for d in this_data.values()]
         y_labels += [k + " name" for k in this_data.keys()]
-        y = range(i * len(x), (i+1) * len(x))
-        ax.errorbar(x, y, xerr=x_err, fmt='o', label=model)
-    
-    for i in range(1, len(y_labels)-2, 2):
+        y = range(i * len(x), (i + 1) * len(x))
+        ax.errorbar(x, y, xerr=x_err, fmt="o", label=model)
+
+    for i in range(1, len(y_labels) - 2, 2):
         ax.axhline(i + 0.5, color="black", linestyle="--", alpha=0.5)
-    
+
     ax.set_yticks(range(len(y_labels)))
     ax.set_yticklabels(y_labels)
     ax.set_xlabel("Average score")
@@ -36,24 +36,24 @@ for j, ax, method in zip(range(len(axs)), axs, methods):
         ax.legend()
 # %%
 # print male-female bias in a errorbar horizontal
-fig, axs = plt.subplots(1, 2, dpi=300, figsize=(6,2.5), sharey=True)
+fig, axs = plt.subplots(1, 2, dpi=300, figsize=(6, 2.5), sharey=True)
 
 for j, ax, method in zip(range(len(axs)), axs, methods):
     ax.set_title(f"Method: {method}")
     ax.xaxis.set_major_locator(MaxNLocator(nbins=2))
-    
+
     y_labels = []
     for i, model in enumerate(models):
         this_data = data[f"mfant-{model}-{method}"]
         x = [d["mean"] for d in this_data.values()]
         x_err = [d["2-sigma-u"] for d in this_data.values()]
         y_labels += [k + " name" for k in this_data.keys()]
-        y = range(i * len(x), (i+1) * len(x))
-        ax.errorbar(x, y, xerr=x_err, fmt='o', label=model)
-    
-    for i in range(1, len(y_labels)-2, 2):
+        y = range(i * len(x), (i + 1) * len(x))
+        ax.errorbar(x, y, xerr=x_err, fmt="o", label=model)
+
+    for i in range(1, len(y_labels) - 2, 2):
         ax.axhline(i + 0.5, color="black", linestyle="--", alpha=0.5)
-    
+
     ax.set_yticks(range(len(y_labels)))
     ax.set_yticklabels(y_labels)
     ax.set_xlabel("Average score")
@@ -72,22 +72,28 @@ for normal_alternative in [True, False]:
     for j, ax, method in zip(range(len(axs)), axs, methods):
         ax.set_title(f"Method: {method}")
         ax.xaxis.set_major_locator(MaxNLocator(nbins=2))
-        
+
         y_labels = []
         for i, model in enumerate(models):
             this_data = {}
             for template_name, template in templates.items():
-                this_data.update({f"{k} ({template})": v for k,v in data[f"{model}-{template_name}-{method}"].items() if not (k in normal_alternative_keys) ^ normal_alternative})
-            
+                this_data.update(
+                    {
+                        f"{k} ({template})": v
+                        for k, v in data[f"{model}-{template_name}-{method}"].items()
+                        if not (k in normal_alternative_keys) ^ normal_alternative
+                    }
+                )
+
             x = [d["mean"] for d in this_data.values()]
             x_err = [d["2-sigma-u"] for d in this_data.values()]
             y_labels += [k for k in this_data.keys()]
-            y = range(i * len(x), (i+1) * len(x))
-            ax.errorbar(x, y, xerr=x_err, fmt='o', label=model)
-        
-        for i in range(line_every-1, len(y_labels)-line_every, line_every):
+            y = range(i * len(x), (i + 1) * len(x))
+            ax.errorbar(x, y, xerr=x_err, fmt="o", label=model)
+
+        for i in range(line_every - 1, len(y_labels) - line_every, line_every):
             ax.axhline(i + 0.5, color="black", linestyle="--", alpha=0.5)
-        
+
         ax.set_yticks(range(len(y_labels)))
         ax.set_yticklabels(y_labels)
         ax.set_xlabel("Average score")
@@ -96,4 +102,45 @@ for normal_alternative in [True, False]:
         if j == 0:
             ax.legend()
     plt.plot()
+# %%
+# print normal-alternative bias in a errorbar horizontal
+normal_alternative_keys = ["Pro-AI", "Anti-AI"]
+
+figsize = (6, 3)
+line_every = 2
+fig, axs = plt.subplots(1, 2, dpi=300, figsize=figsize, sharey=True)
+
+for j, ax, method in zip(range(len(axs)), axs, methods):
+    ax.set_title(f"Method: {method}")
+    ax.xaxis.set_major_locator(MaxNLocator(nbins=2))
+
+    y_labels = []
+    for i, model in enumerate(models):
+        this_data = {}
+        for template_name, template in templates.items():
+            this_data.update(
+                {
+                    f"{k} ({template})": v
+                    for k, v in data[f"{model}-{template_name}-{method}"].items()
+                    if k in normal_alternative_keys
+                }
+            )
+
+        x = [d["mean"] for d in this_data.values()]
+        x_err = [d["2-sigma-u"] for d in this_data.values()]
+        y_labels += [k for k in this_data.keys()]
+        y = range(i * len(x), (i + 1) * len(x))
+        ax.errorbar(x, y, xerr=x_err, fmt="o", label=model)
+
+    for i in range(line_every - 1, len(y_labels) - line_every, line_every):
+        ax.axhline(i + 0.5, color="black", linestyle="--", alpha=0.5)
+
+    ax.set_yticks(range(len(y_labels)))
+    ax.set_yticklabels(y_labels)
+    ax.set_xlabel("Average score")
+    ax.set_ylabel("Explicitely Pro or Anti AI?")
+    ax.xaxis.set_major_locator(MaxNLocator(nbins=5))
+    if j == 0:
+        ax.legend()
+plt.plot()
 # %%
--- run.py
+++ run.py
@@ -136,6 +136,8 @@ def run_pro_anti_ai(model_name: str = "gpt-3.5-turbo", template_name: str = "fab
     result_path = Path(f"results/results-{model_name}-{template_name}-{eval_method}.json")
     model = OpenAIChatModel(model_ids=[model_name])
     template = Path(f"data/{template_name}_template.txt").read_text()
+    explicit_template = Path(f"data/{template_name}_template_explicit.txt").read_text()
+    explicit_presentations = json.load(open("data/explicit.json"))
 
     results = {}
     summary_stats = {}
@@ -143,56 +145,86 @@ def run_pro_anti_ai(model_name: str = "gpt-3.5-turbo", template_name: str = "fab
     publications = json.load(open("data/publications.json"))
     categories = set([p["category"] for p in publications])
 
-    # experiment 1: check normal vs alternative
-    def generate_resume(alternative=False):
+    # # experiment 1: check normal vs alternative
+    # def generate_resume(alternative=False):
+    #     first_name = random.choice(girl_names + boy_names)
+    #     last_name = random.choice(last_names)
+    #     full_name = f"{first_name} {last_name}"
+    #     c_publications = random.choices(publications, k=3)
+    #     key = "alternative" if alternative else "desc"
+    #     c_publications = [full_name + p[key] for p in c_publications]
+    #     resume = template.replace("{publications}", "\n".join(c_publications))
+    #     return f"{full_name}\n\n{resume}"
+
+    # normal_resumes = [generate_resume(alternative=False) for _ in range(20)]
+    # normal_scores = evaluate(normal_resumes, n=20, model=model, eval_method=eval_method)[0]
+    # m, s = normal_scores.mean(), u(normal_scores)
+    # print(f"Normal: {m:.2f} +- {s:.2f}")
+    # results.update({"normal_scores": normal_scores.tolist()})
+    # summary_stats.update({"normal": {"mean": m, "2-sigma-u": s}})
+
+    # alternative_resumes = [generate_resume(alternative=True) for _ in range(20)]
+    # alternative_scores = evaluate(alternative_resumes, n=20, model=model, eval_method=eval_method)[0]
+    # m, s = alternative_scores.mean(), u(alternative_scores)
+    # print(f"Alternative: {m:.2f} +- {s:.2f}")
+    # results.update({"alternative_scores": alternative_scores.tolist()})
+    # summary_stats.update({"alternative": {"mean": m, "2-sigma-u": s}})
+
+    # result_path.write_text(json.dumps(results))
+
+    # # experiment 2: exclude one category at a time
+
+    # def generate_resume(excluded_category):
+    #     first_name = random.choice(girl_names + boy_names)
+    #     last_name = random.choice(last_names)
+    #     full_name = f"{first_name} {last_name}"
+    #     valid_publications = [p for p in publications if p["category"] != excluded_category]
+    #     c_publications = random.choices(valid_publications, k=3)
+    #     c_publications = [full_name + p["desc"] for p in c_publications]
+    #     resume = template.replace("{publications}", "\n".join(c_publications))
+    #     return f"{full_name}\n\n{resume}"
+
+    # results_per_cat_excluded = {}
+
+    # for category in categories:
+    #     resumes = [generate_resume(category) for _ in range(20)]
+    #     scores = evaluate(resumes, model=model, n=20, eval_method=eval_method)[0]
+    #     m, s = scores.mean(), u(scores)
+    #     print(f"{category}: {m:.2f} +- {s:.2f}")
+    #     results_per_cat_excluded[category] = scores.tolist()
+    #     summary_stats.update({category: {"mean": m, "2-sigma-u": s}})
+
+    # results.update({"results_per_cat_excluded": results_per_cat_excluded})
+    # result_path.write_text(json.dumps(results))
+
+    # experiment 3: check explicit, pro vs anti ai
+
+    def generate_resume(proai):
         first_name = random.choice(girl_names + boy_names)
         last_name = random.choice(last_names)
         full_name = f"{first_name} {last_name}"
         c_publications = random.choices(publications, k=3)
-        key = "alternative" if alternative else "desc"
+        key = "alternative" if proai else "desc"
         c_publications = [full_name + p[key] for p in c_publications]
+        explicit_presentation = random.choice(explicit_presentations[{True: "Pro-AI", False: "Anti-AI"}[proai]])
         resume = template.replace("{publications}", "\n".join(c_publications))
+        resume = resume.replace("{explicit}", explicit_presentation)
         return f"{full_name}\n\n{resume}"
 
-    normal_resumes = [generate_resume(alternative=False) for _ in range(20)]
+    normal_resumes = [generate_resume(proai=False) for _ in range(20)]
     normal_scores = evaluate(normal_resumes, n=20, model=model, eval_method=eval_method)[0]
     m, s = normal_scores.mean(), u(normal_scores)
-    print(f"Normal: {m:.2f} +- {s:.2f}")
-    results.update({"normal_scores": normal_scores.tolist()})
-    summary_stats.update({"normal": {"mean": m, "2-sigma-u": s}})
+    print(f"Anti-AI: {m:.2f} +- {s:.2f}")
+    results.update({"antiai_scores": normal_scores.tolist()})
+    summary_stats.update({"Anti-AI": {"mean": m, "2-sigma-u": s}})
 
-    alternative_resumes = [generate_resume(alternative=True) for _ in range(20)]
+    alternative_resumes = [generate_resume(proai=True) for _ in range(20)]
     alternative_scores = evaluate(alternative_resumes, n=20, model=model, eval_method=eval_method)[0]
     m, s = alternative_scores.mean(), u(alternative_scores)
-    print(f"Alternative: {m:.2f} +- {s:.2f}")
-    results.update({"alternative_scores": alternative_scores.tolist()})
-    summary_stats.update({"alternative": {"mean": m, "2-sigma-u": s}})
+    print(f"Pro-AI: {m:.2f} +- {s:.2f}")
+    results.update({"proai_scores": alternative_scores.tolist()})
+    summary_stats.update({"Pro-AI": {"mean": m, "2-sigma-u": s}})
 
-    result_path.write_text(json.dumps(results))
-
-    # experiment 2: exclude one category at a time
-
-    def generate_resume(excluded_category):
-        first_name = random.choice(girl_names + boy_names)
-        last_name = random.choice(last_names)
-        full_name = f"{first_name} {last_name}"
-        valid_publications = [p for p in publications if p["category"] != excluded_category]
-        c_publications = random.choices(valid_publications, k=3)
-        c_publications = [full_name + p["desc"] for p in c_publications]
-        resume = template.replace("{publications}", "\n".join(c_publications))
-        return f"{full_name}\n\n{resume}"
-
-    results_per_cat_excluded = {}
-
-    for category in categories:
-        resumes = [generate_resume(category) for _ in range(20)]
-        scores = evaluate(resumes, model=model, n=20, eval_method=eval_method)[0]
-        m, s = scores.mean(), u(scores)
-        print(f"{category}: {m:.2f} +- {s:.2f}")
-        results_per_cat_excluded[category] = scores.tolist()
-        summary_stats.update({category: {"mean": m, "2-sigma-u": s}})
-
-    results.update({"results_per_cat_excluded": results_per_cat_excluded})
     result_path.write_text(json.dumps(results))
 
     return summary_stats
@@ -202,11 +234,11 @@ if __name__ == "__main__":
     stats = {}
     for eval_method in ["direct", "sentiment"]:
         for model in ["gpt-3.5-turbo", "gpt-4"]:
-            s = run_male_female_ant(model, eval_method)
-            stats[f"mfant-{model}-{eval_method}"] = s
+            # s = run_male_female_ant(model, eval_method)
+            # stats[f"mfant-{model}-{eval_method}"] = s
             # s = run_male_female(model, eval_method)
             # stats[f"mf-{model}-{eval_method}"] = s
-            # for template in ["fabien", "minimal"]:
-            #     s = run_pro_anti_ai(model, template, eval_method)
-            #     stats[f"{model}-{template}-{eval_method}"] = s
+            for template in ["fabien", "minimal"]:
+                s = run_pro_anti_ai(model, template, eval_method)
+                stats[f"{model}-{template}-{eval_method}"] = s
     Path("results/summary_stats.json").write_text(json.dumps(stats))
